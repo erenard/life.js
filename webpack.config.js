9f42c046d7e48686ba564b0b01321788d5aa7b60
Eric RENARD <timmy469@gmail.com>
2017-03-28 15:57:16 +0200

    Minor improvement

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var path = require('path');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
 const webpack = require('webpack'); //to access built-in plugins
 
