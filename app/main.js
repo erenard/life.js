5dca79c47bd6c7962c0204582f1cae9d4458308d
Eric RENARD <timmy469@gmail.com>
2017-12-05 00:00:58 +0100

    Update dependencies, adds vue.js

--- app/main.js
+++ app/main.js
@@ -1,26 +1,25 @@
-import 'app.css';
-import UserInterface from 'user-interface';
-import Grid from 'grid';
-import Renderer from 'renderer';
-import Animation from 'animation';
+import 'app.css'
+import UserInterface from 'user-interface'
+import Grid from 'grid'
+import Renderer from 'renderer'
+import Animation from 'animation'
 
+function main (setup) {
+  var radius = setup.lifeCellSize || 4,
+    width = 1280,
+    height = 1024,
+    viewport = document.getElementById('viewport'),
+    grid = new Grid(Math.floor(width / radius), Math.floor(height / radius)),
+    renderer = new Renderer(width, height, viewport, grid, radius),
+    animation = Animation(() => {
+      // This function will wrap the whole process of updating the game and drawing it
+      renderer.render()
+      grid.update()
+    })
 
-function main(setup) {
-	var radius = setup.lifeCellSize || 4,
-		width = 1280,
-		height = 1024,
-		viewport = document.getElementById('viewport'),
-		grid = new Grid(Math.floor(width / radius), Math.floor(height / radius)),
-		renderer = new Renderer(width, height, viewport, grid, radius),
-		animation = Animation(() => {
-			// This function will wrap the whole process of updating the game and drawing it
-			renderer.render();
-			grid.update();
-		});
-
-	new UserInterface(grid, animation);
-	grid.random(0.30);
-	animation.start();
+  new UserInterface(grid, animation)
+  grid.random(0.30)
+  animation.start()
 }
 
-main({lifeCellSize: 4});
+main({lifeCellSize: 4})
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,43 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-//const webpack = require('webpack'); //to access built-in plugins
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin') // installed via npm
+// const webpack = require('webpack'); //to access built-in plugins
 
 const config = {
-	entry: './app/main.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.join(__dirname, 'dist')
-	},
-	resolve: {
-		modules: [
-			'app',
-			'node_modules'
-		]
-	},
-	plugins: [
-		//new webpack.optimize.UglifyJsPlugin(),
-		new HtmlWebpackPlugin({template: './app/index.html'})
-	],
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				loader: 'babel-loader'
-			},
-			{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
-			//{ test: /\.png$/, loader: 'url-loader?limit=100000' },
-			//{ test: /\.jpg$/, loader: 'file-loader' },
-			{ test: /\.ttf$/, loader: 'file-loader' },
-		]
-	},
-	devtool: 'source-map',
-	devServer: {
-		compress: false,
-		port: 9000
-	},
-	watch: false,
-	watchOptions: {
-		ignore: '/node_modules'
-	}
-};
+  entry: './app/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  resolve: {
+    modules: [
+      'app',
+      'node_modules'
+    ],
+    alias: { vue: 'vue/dist/vue.js' }
+  },
+  plugins: [
+    // new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({ template: './app/index.html' })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      },
+      { test: /\.vue$/, loader: 'vue-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      // { test: /\.png$/, loader: 'url-loader?limit=100000' },
+      // { test: /\.jpg$/, loader: 'file-loader' },
+      { test: /\.ttf$/, loader: 'file-loader' }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    compress: false,
+    port: 9000
+  }
+}
 
-module.exports = config;
+module.exports = config
