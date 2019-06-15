acdabd60d00a49c6a8f8abe07852849e38fbb118
Eric Renard <timmy469@gmail.com>
2019-06-15 08:50:59 +0200

    Fix transformation matrix

--- app/render/cell-renderer.js
+++ app/render/cell-renderer.js
@@ -13,9 +13,8 @@ uniform mat4 uTranslationMatrix;
 varying float vAlpha;
 
 void main(void) {
-  gl_Position = uTranslationMatrix * vec4(aVertexPosition, 0.0, 1.0);
   vAlpha = aVertexAlpha;
-  // gl_Position = vec4(aVertexPosition, 0.0, 1.0);
+  gl_Position = uTranslationMatrix * vec4(aVertexPosition, 0.0, 1.0);
   gl_PointSize = uPointSize;
 }`
 const fDraw = `
@@ -24,7 +23,7 @@ precision mediump float;
 varying float vAlpha;
 
 void main(void) {
-  gl_FragColor = vec4(0.5, 0.5, 0.5, vAlpha);
+  gl_FragColor = vec4(1.0, 0.0, 0.0, vAlpha);
 }`
 
 /**
@@ -37,6 +36,9 @@ void main(void) {
  * @param {number} cellSize - Cell sprite's size.
  */
 export default function CellRenderer (width, height, viewport, grid, cellSize) {
+  // width = 200
+  // height = 200
+  cellSize = 10
   const canvas = document.createElement('canvas')
   canvas.height = height
   canvas.width = width
@@ -44,17 +46,15 @@ export default function CellRenderer (width, height, viewport, grid, cellSize) {
 
   const gl = twgl.getContext(canvas, { depth: false, antialiasing: false })
   const programDraw = twgl.createProgramInfo(gl, [vDraw, fDraw])
-  const n = width / cellSize
-  const m = height / cellSize
+  const columnCount = width / cellSize
+  const rowCount = height / cellSize
   const pointPosition = []
   const pointAlpha = []
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      // pointPosition.push(i / (n - 1))
-      // pointPosition.push(j / (m - 1))
-      pointPosition.push(i)
-      pointPosition.push(j)
-      pointAlpha.push((i + j) % 2)
+  for (let column = 0; column < columnCount; column++) {
+    for (let row = 0; row < rowCount; row++) {
+      pointPosition.push(column)
+      pointPosition.push(row)
+      pointAlpha.push((column + row) % 2)
     }
   }
   // pointPosition.push(1, 0)
@@ -65,13 +65,14 @@ export default function CellRenderer (width, height, viewport, grid, cellSize) {
   const pointsBuffer = twgl.createBufferInfoFromArrays(gl, pointsObject)
 
   const translationMatrix = twgl.m4.identity()
-  twgl.m4.scale(translationMatrix, [cellSize / (0.5 * width), -cellSize / (0.5 * height), 1.0], translationMatrix)
-  twgl.m4.translate(translationMatrix, [-width / 4, -height / 4, 0.0], translationMatrix)
+  twgl.m4.translate(translationMatrix, [-1, 1, 0], translationMatrix)
+  twgl.m4.scale(translationMatrix, [(2 * cellSize / width), (-2 * cellSize / height), 1.0], translationMatrix)
+  twgl.m4.translate(translationMatrix, [0.5, 0.5, 0], translationMatrix)
 
   gl.clearColor(0, 0, 0, 1)
   gl.useProgram(programDraw.program)
   twgl.setUniforms(programDraw, {
-    uPointSize: 5.0,
+    uPointSize: cellSize,
     uTranslationMatrix: translationMatrix
   })
 
@@ -82,6 +83,7 @@ export default function CellRenderer (width, height, viewport, grid, cellSize) {
     render () {
       gl.clear(gl.COLOR_BUFFER_BIT)
       // twgl.resizeCanvasToDisplaySize(gl.canvas)
+      // console.log(gl.canvas.width, gl.canvas.height)
       // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
       // drawing particles
       twgl.setBuffersAndAttributes(gl, programDraw, pointsBuffer)
