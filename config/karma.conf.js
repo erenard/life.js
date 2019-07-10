9bdf7b780e31eff7426278f3a46e3991eb4634ab
Eric Renard <timmy469@gmail.com>
2019-07-10 16:09:03 +0200

    Removes the old renderer

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -15,7 +15,6 @@ module.exports = function (config) {
       // { pattern: './test/**/*.spec.js', watched: false }
       './test/game/cell.spec.js',
       './test/game/grid.spec.js',
-      './test/render/pixi-renderer.spec.js',
       './test/render/animation.spec.js',
       './test/components/rules-editor-vue.spec.js',
       './test/components/user-interface-vue.spec.js'
@@ -25,7 +24,6 @@ module.exports = function (config) {
       // './test/**/*.spec.js': ['webpack']
       './test/game/cell.spec.js': ['webpack'],
       './test/game/grid.spec.js': ['webpack'],
-      './test/render/pixi-renderer.spec.js': ['webpack'],
       './test/render/animation.spec.js': ['webpack'],
       './test/components/rules-editor-vue.spec.js': ['webpack'],
       './test/components/user-interface-vue.spec.js': ['webpack']
