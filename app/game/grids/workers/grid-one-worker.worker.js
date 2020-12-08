8242cb078c8084295a8acdbaa70b80b7b5c2c0a8
Éric Renard <timmy469@gmail.com>
2020-12-08 14:46:50 +0100

    feat(grid-one-worker.worker): improve testability

--- app/game/grids/workers/grid-one-worker.worker.js
+++ app/game/grids/workers/grid-one-worker.worker.js
@@ -1,22 +1,46 @@
 import update from '../../grid-updater.js'
 
-let cells, sizeX, length, rules
-
-addEventListener('message', event => {
-  const type = event.data.type
-  const data = event.data
-  switch (type) {
-    case 'init':
-      cells = new Uint8Array(data.buffer)
-      sizeX = data.board.gridWidth
-      length = sizeX * sizeX
-      rules = data.rules
-      break
-    case 'update':
-      update(cells, length, sizeX, rules)
-      postMessage({ type: 'render' })
-      break
-    case 'rules':
-      rules = data.rules
+export class GridWebWorker {
+  constructor () {
+    this.cells = null
+    this.sizeX = null
+    this.length = null
+    this.rules = null
   }
-})
+
+  onInit (data) {
+    this.cells = new Uint8Array(data.buffer)
+    this.sizeX = data.board.gridWidth
+    this.length = data.board.gridWidth * data.board.gridHeight
+    this.rules = data.rules
+  }
+
+  onUpdate (data) {
+    update(this.cells, this.length, this.sizeX, this.rules)
+    postMessage({ type: 'render' })
+  }
+
+  onRules (data) {
+    this.rules = data.rules
+  }
+
+  onMessage (event) {
+    const type = event.data.type
+    const data = event.data
+    switch (type) {
+      case 'init':
+        this.onInit(data)
+        break
+      case 'update':
+        this.onUpdate(data)
+        break
+      case 'rules':
+        this.onRules(data)
+        break
+    }
+  }
+}
+
+const worker = new GridWebWorker()
+
+addEventListener('message', worker.onMessage.bind(worker))
